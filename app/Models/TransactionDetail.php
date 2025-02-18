5ef3520466094807802024f11880a9dd19a3ed0b
Isep Lutpi Nur (upi) <iseplutpinur7@gmail.com>
2025-02-19 03:31:36 +0700

    feat: penyesuaian model

--- app/Models/TransactionDetail.php
+++ app/Models/TransactionDetail.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TransactionDetail extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['id_transaksi', 'id_produk', 'quantity'];
+    
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'id_produk');
+    }
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'id_transaksi');
+    }
 }
